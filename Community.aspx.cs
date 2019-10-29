9e83b15e7203f4e4121f19d071f688429febff92
Shital Chaudhary <39867916+CHAUDHARYS1@users.noreply.github.com>
2019-10-29 10:06:31 -0500

    Add files via upload

---
+++ Community.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Community : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["currentUserID"] == null)   //redirect to login if no on is logged in
+        { Response.Redirect("Login.aspx"); }
+    }
+}
---
+++ Index.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if ((Session["currentUserID"] == null) || (Session["currentUserRole"].ToString() != "Student"))  //redirect to login if no on is logged in
+        { Response.Redirect("Login.aspx"); }
+
+        //fill in first and/or last name if there is one in the database for current user
+        string nameConcat = "";
+        if (Session["currentUserFirstName"] != null)
+        { nameConcat += nameConcat = Session["currentUserFirstName"].ToString() + " "; }
+        if (Session["currentUserLastName"] != null)
+        { nameConcat += Session["currentUserLastName"].ToString(); }
+        lblUserName.Text = nameConcat;
+    }
+}
